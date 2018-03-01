aa439f82908b8d67b57bd5118249abc0d50e5cdd
zhuxiaohui <zxh1786619259@163.com>
2018-03-01 13:49:16 +0800

    新增加PersonInfo实体类

---
+++ app/src/main/java/com/gbf/gitdemo/PersonInfo.java
@@ -0,0 +1,17 @@
+package com.gbf.gitdemo;
+
+/**
+ * Created by zhuxiaohui on 2018/3/1.
+ */
+
+public class PersonInfo {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
